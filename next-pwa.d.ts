1ec8d5bff33ca665be972f4ca88499c36848475b
matias.elias <38197440+lucasmaksymon@users.noreply.github.com>
2026-01-31 13:09:41 -0300

    oka

---
+++ next-pwa.d.ts
@@ -0,0 +1,17 @@
+declare module 'next-pwa' {
+  import { NextConfig } from 'next';
+  
+  interface PWAConfig {
+    dest?: string;
+    disable?: boolean;
+    register?: boolean;
+    skipWaiting?: boolean;
+    runtimeCaching?: any[];
+    buildExcludes?: string[];
+    [key: string]: any;
+  }
+  
+  function withPWA(config: PWAConfig): (nextConfig: NextConfig) => NextConfig;
+  
+  export default withPWA;
+}
